995494c386be4492ae9dcb495dd7e52eb68e10f2
yto <yangtaocsmz@gmail.com>
2018-01-29 17:27:53 +0800

    vue-template login demo

--- vueAdmin-template/src/views/layout/Layout.vue
+++ vueAdmin-template/src/views/layout/Layout.vue
@@ -3,20 +3,22 @@
     <sidebar class="sidebar-container"></sidebar>
     <div class="main-container">
       <navbar></navbar>
+      <tags-view></tags-view>
       <app-main></app-main>
     </div>
   </div>
 </template>
 
 <script>
-import { Navbar, Sidebar, AppMain } from '@/views/layout/components'
+import { Navbar, Sidebar, AppMain, TagsView } from '@/views/layout/components'
 
 export default {
   name: 'layout',
   components: {
     Navbar,
     Sidebar,
-    AppMain
+    AppMain,
+    TagsView
   },
   computed: {
     sidebar() {
